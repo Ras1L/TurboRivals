b5cd416c8fb211805cb915682b33ced3418d1176
Ras1L <gasharov06@gmail.com>
2026-03-02 21:22:40 +0300

    chore: including enet

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,25 @@
 #include "raylib.h"
+#include <cstdlib>
+extern "C" { 
+    #include <enet/enet.h>
+}
+#include <iostream>
 
-const int SCREEN_WIDTH = 1920;
+const int SCREEN_WIDTH  = 1920;
 const int SCREEN_HEIGHT = 1080;
-const int TARGET_FPS = 60;
+const int TARGET_FPS    = 60;
 
 void UpdateDrawFrame();
 
 int main()
 {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "TurboRivals");
-    
     SetTargetFPS(TARGET_FPS);
+
+    if (enet_initialize() != 0) {
+        std::cerr << "An error occurred while initializing ENet.\n";
+        return EXIT_FAILURE;
+    }
 
     while (!WindowShouldClose())
     {
@@ -19,7 +28,9 @@ int main()
 
     CloseWindow();
 
-    return 0;
+    atexit(enet_deinitialize);
+
+    return EXIT_SUCCESS;
 }
 
 void UpdateDrawFrame()
